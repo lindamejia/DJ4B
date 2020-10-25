72aa22c71a85eec2ddbc1f8e0e8fcc9ac705642a
lindamejia <lindaisamarmejia@gmail.com>
2020-10-25 12:54:54 -0700

    Fixed Package json

---
+++ build/precache-manifest.eed54d0da8439f333d333b2305514411.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7183788f740e81302f225a51803971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ed434d2984cdf45e919",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "03487bffab08f8b872bf",
+    "url": "/static/css/main.ca64e10b.chunk.css"
+  },
+  {
+    "revision": "8ed434d2984cdf45e919",
+    "url": "/static/js/2.57efb758.chunk.js"
+  },
+  {
+    "revision": "03487bffab08f8b872bf",
+    "url": "/static/js/main.bb952ca3.chunk.js"
+  },
+  {
+    "revision": "28518cccfb15e06768b7",
+    "url": "/static/js/runtime-main.107c009e.js"
+  },
+  {
+    "revision": "d62054fa40a7080cd18cac6469b8897a",
+    "url": "/static/media/logo.d62054fa.png"
+  },
+  {
+    "revision": "e1ebc9bcd74c39e97e98bb45a950bccf",
+    "url": "/static/media/main-new.e1ebc9bc.jpg"
+  },
+  {
+    "revision": "9743b3422d410d62909da6c6fdf5db37",
+    "url": "/static/media/nvr2.9743b342.png"
+  },
+  {
+    "revision": "df65991d2a029ebe85d9139d0aeb222c",
+    "url": "/static/media/press.df65991d.jpg"
+  },
+  {
+    "revision": "02d958ad20850902f7d3368feb5f0b47",
+    "url": "/static/media/tour.02d958ad.jpg"
+  }
+]);
