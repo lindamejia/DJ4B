1d0af776c3b6587e6f876e790cb2b891ee356cfe
lindamejia <lindaisamarmejia@gmail.com>
2020-10-26 15:14:12 -0700

    Fixed header responsiveness

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,6 +7,9 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
+  Col,
+  Container,
+  Row,
 } from "reactstrap";
 import logo from "../../images/logo.png";
 import whiteLogo from "../../images/4bwhite.png";
@@ -82,157 +85,165 @@ export default class Example extends React.Component {
           fixed="top"
           className={style.navbar}
         >
-          <NavbarToggler onClick={this.toggle} className={style.toggler} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav navbar>
-              <NavItem className={style.nav}>
-                <Link
-                  className="reactscrollLink"
-                  style={{ color: this.state.color }}
-                  activeClass="active"
-                  to="home"
-                  spy={true}
-                  smooth={true}
-                  offset={50}
-                  duration={500}
-                >
-                  HOME
-                </Link>
-              </NavItem>
-              <NavItem className={style.nav}>
-                <Link
-                  className="reactscrollLink"
-                  style={{ color: this.state.color }}
-                  activeClass="active"
-                  to="tour"
-                  spy={true}
-                  smooth={true}
-                  offset={-200}
-                  duration={500}
-                >
-                  TOUR
-                </Link>
-              </NavItem>
-              <NavItem className={style.nav}>
-                <Link
-                  className="reactscrollLink"
-                  style={{ color: this.state.color }}
-                  activeClass="active"
-                  to="music"
-                  spy={true}
-                  smooth={true}
-                  duration={500}
-                >
-                  MUSIC
-                </Link>
-              </NavItem>
-              <NavItem className={style.nav}>
-                <Link
-                  className="reactscrollLink"
-                  style={{ color: this.state.color }}
-                  activeClass="active"
-                  to="merch"
-                  spy={true}
-                  smooth={true}
-                  offset={2000}
-                  duration={500}
-                >
-                  MERCH
-                </Link>
-              </NavItem>
-              <NavItem className={style.nav}>
-                <Link
-                  className="reactscrollLink"
-                  style={{ color: this.state.color }}
-                  activeClass="active"
-                  to="contact"
-                  spy={true}
-                  smooth={true}
-                  offset={2000}
-                  duration={500}
-                >
-                  CONTACT
-                </Link>
-              </NavItem>
-            </Nav>
-          </Collapse>
-          <NavbarBrand className="d-lg-block">
-            <img
-              src={this.state.logo}
-              alt="Logo"
-              className={style.logo}
-              onClick={this.scrollToTop}
-            />
-          </NavbarBrand>
-          {this.state.color === "white" ? (
-            <div>
-              <a href="https://www.facebook.com/LIKE4B/" target="_blank">
-                <img src={facebookWhite} className={style.socials} />
-              </a>
+          <Container className="justify-content-center">
+            <Col md={{ size: 5, offset: 1 }} xs={{ size:6 }}>
+              <NavbarToggler onClick={this.toggle} className={style.toggler} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav navbar>
+                  <NavItem className={style.nav}>
+                    <Link
+                      className="reactscrollLink"
+                      style={{ color: this.state.color }}
+                      activeClass="active"
+                      to="home"
+                      spy={true}
+                      smooth={true}
+                      offset={50}
+                      duration={500}
+                    >
+                      HOME
+                    </Link>
+                  </NavItem>
+                  <NavItem className={style.nav}>
+                    <Link
+                      className="reactscrollLink"
+                      style={{ color: this.state.color }}
+                      activeClass="active"
+                      to="tour"
+                      spy={true}
+                      smooth={true}
+                      offset={-200}
+                      duration={500}
+                    >
+                      TOUR
+                    </Link>
+                  </NavItem>
+                  <NavItem className={style.nav}>
+                    <Link
+                      className="reactscrollLink"
+                      style={{ color: this.state.color }}
+                      activeClass="active"
+                      to="music"
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                    >
+                      MUSIC
+                    </Link>
+                  </NavItem>
+                  <NavItem className={style.nav}>
+                    <Link
+                      className="reactscrollLink"
+                      style={{ color: this.state.color }}
+                      activeClass="active"
+                      to="merch"
+                      spy={true}
+                      smooth={true}
+                      offset={2000}
+                      duration={500}
+                    >
+                      MERCH
+                    </Link>
+                  </NavItem>
+                  <NavItem className={style.nav}>
+                    <Link
+                      className="reactscrollLink"
+                      style={{ color: this.state.color }}
+                      activeClass="active"
+                      to="contact"
+                      spy={true}
+                      smooth={true}
+                      offset={2000}
+                      duration={500}
+                    >
+                      CONTACT
+                    </Link>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Col>
+            <Col md={{ size: 2, offset: 1 }} xs={{ size:3, offset:3 }} >
+              <NavbarBrand className="d-lg-block">
+                <img
+                  src={this.state.logo}
+                  alt="Logo"
+                  className={style.logo}
+                  onClick={this.scrollToTop}
+                />
+              </NavbarBrand>
+            </Col>
+            <Col md={{ size: 5, offset: 1 }} xs={{ size:11, offset:1 }}>
+              {this.state.color === "white" ? (
+                <div className={style.socialMedia}>
+                  <a href="https://www.facebook.com/LIKE4B/" target="_blank">
+                    <img src={facebookWhite} className={style.socials} />
+                  </a>
 
-              <a href="https://www.instagram.com/dj4b/" target="_blank">
-                <img src={instagramWhite} className={style.socials} />
-              </a>
+                  <a href="https://www.instagram.com/dj4b/" target="_blank">
+                    <img src={instagramWhite} className={style.socials} />
+                  </a>
 
-              <a
-                href="https://www.youtube.com/user/THEREALDJ4B/videos"
-                target="_blank"
-              >
-                <img src={youtubeWhite} className={style.socials} />
-              </a>
+                  <a
+                    href="https://www.youtube.com/user/THEREALDJ4B/videos"
+                    target="_blank"
+                  >
+                    <img src={youtubeWhite} className={style.socials} />
+                  </a>
 
-              <a href="https://twitter.com/DJ4B" target="_blank">
-                <img src={twitterWhite} className={style.socials} />
-              </a>
+                  <a href="https://twitter.com/DJ4B" target="_blank">
+                    <img src={twitterWhite} className={style.socials} />
+                  </a>
 
-              <a href="https://soundcloud.com/dj4b" target="_blank">
-                <img src={soundcloudWhite} className={style.socials} />
-              </a>
+                  <a href="https://soundcloud.com/dj4b" target="_blank">
+                    <img src={soundcloudWhite} className={style.socials} />
+                  </a>
 
-              <iframe
-                src="https://embed.spotify.com/follow/1/?uri=spotify:artist:0LIl9fjMPEZp8UDiL8Yuo4&amp;size=basic&amp;theme=light&amp;show-count=0"
-                width="100"
-                height="25"
-                scrolling="no"
-                frameBorder="0"
-                allowtransparency="true"
-              ></iframe>
-            </div>
-          ) : (
-            <div>
-              <a href="https://www.facebook.com/LIKE4B/" target="_blank">
-                <img src={facebook} className={style.socials} />
-              </a>
+                  <iframe
+                    src="https://embed.spotify.com/follow/1/?uri=spotify:artist:0LIl9fjMPEZp8UDiL8Yuo4&amp;size=basic&amp;theme=light&amp;show-count=0"
+                    width="100"
+                    height="25"
+                    scrolling="no"
+                    frameBorder="0"
+                    allowtransparency="true"
+                  ></iframe>
+                </div>
+              ) : (
+                <div className={style.socialMedia}>
+                  <a href="https://www.facebook.com/LIKE4B/" target="_blank">
+                    <img src={facebook} className={style.socials} />
+                  </a>
 
-              <a href="https://www.instagram.com/dj4b/" target="_blank">
-                <img src={instagram} className={style.socials} />
-              </a>
+                  <a href="https://www.instagram.com/dj4b/" target="_blank">
+                    <img src={instagram} className={style.socials} />
+                  </a>
 
-              <a
-                href="https://www.youtube.com/user/THEREALDJ4B/videos"
-                target="_blank"
-              >
-                <img src={youtube} className={style.socials} />
-              </a>
+                  <a
+                    href="https://www.youtube.com/user/THEREALDJ4B/videos"
+                    target="_blank"
+                  >
+                    <img src={youtube} className={style.socials} />
+                  </a>
 
-              <a href="https://twitter.com/DJ4B" target="_blank">
-                <img src={twitter} className={style.socials} />
-              </a>
+                  <a href="https://twitter.com/DJ4B" target="_blank">
+                    <img src={twitter} className={style.socials} />
+                  </a>
 
-              <a href="https://soundcloud.com/dj4b" target="_blank">
-                <img src={soundcloud} className={style.socials} />
-              </a>
+                  <a href="https://soundcloud.com/dj4b" target="_blank">
+                    <img src={soundcloud} className={style.socials} />
+                  </a>
 
-              <iframe
-                src="https://embed.spotify.com/follow/1/?uri=spotify:artist:0LIl9fjMPEZp8UDiL8Yuo4&amp;size=basic&amp;theme=light&amp;show-count=0"
-                width="100"
-                height="25"
-                scrolling="no"
-                frameBorder="0"
-                allowtransparency="true"
-              ></iframe>
-            </div>
-          )}
+                  <iframe
+                    src="https://embed.spotify.com/follow/1/?uri=spotify:artist:0LIl9fjMPEZp8UDiL8Yuo4&amp;size=basic&amp;theme=light&amp;show-count=0"
+                    width="100"
+                    height="25"
+                    scrolling="no"
+                    frameBorder="0"
+                    allowtransparency="true"
+                  ></iframe>
+                </div>
+              )}
+            </Col>
+          </Container>
         </Navbar>
       </>
     );
