1d7bcb4630055375ad8ab7f85d3f01f6166849dc
lindamejia <lindaisamarmejia@gmail.com>
2021-01-25 19:59:36 -0800

    Added sold out

--- src/components/announcement/Announcement.jsx
+++ src/components/announcement/Announcement.jsx
@@ -1,29 +1,33 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import style from "./Announcement.module.css";
 
-export default class Announcement extends Component {
-  render() {
-    return (
-      <div className={style.background}>
-        <div class={style.sign}>
-          <p class={style.word}>4B & FRIENDS</p>
-        </div>
-        <br />
-        <a
-          href="https://parknrave.frontgatetickets.com/event/mss4t9t7fgkgw7kt"
-          className={style.button}
-          target="_blank"
-        >
-          FRIDAY TICKETS
-        </a>
-        <a
-          href="https://parknrave.frontgatetickets.com/event/s9ep2pja5j7hr45c"
-          className={style.button}
-          target="_blank"
-        >
-          SATURDAY TICKETS
-        </a>
+function Announcement() {
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <div className={style.background}>
+      <div class={style.sign}>
+        <p class={style.word}>4B & FRIENDS</p>
       </div>
-    );
-  }
+      <br />
+      <a
+        href="https://parknrave.frontgatetickets.com/event/mss4t9t7fgkgw7kt"
+        className={style.button}
+        target="_blank"
+      >
+        FRIDAY TICKETS
+      </a>
+      <a
+        href="https://parknrave.frontgatetickets.com/event/s9ep2pja5j7hr45c"
+        className={style.button}
+        target="_blank"
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+      >
+        {isShown ? <span>SOLD OUT</span> : <strike>SATURDAY TICKETS</strike>}
+      </a>
+    </div>
+  );
 }
+
+export default Announcement;
