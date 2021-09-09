618f2a1b6c77a2e282994a45d4526a2653bf2fa0
lindamejia <lindaisamarmejia@gmail.com>
2021-09-08 19:38:39 -0700

    Update image

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import style from "./Featured.module.css";
-import corvette from "../../images/corvette.jpeg";
+import there4u from "../../images/there4u.jpeg";
 
 export default class Featured extends Component {
   render() {
     return (
       <div className={style.featured}>
         <div className={style.gn} id="home">
-          <a href="https://soundcloud.com/dj4b/adderall-4b-x-808gong-remix-1" target="_blank">
-            <img className={style.image} src={corvette} />
+          <a href="https://ffm.to/there4you" target="_blank">
+            <img className={style.image} src={there4u} />
           </a>
         </div>
       </div>
