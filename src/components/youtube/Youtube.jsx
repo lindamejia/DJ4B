cdc6ebfac3548377cec92828c4e5f9eb822e1c07
Linda Mejia <lindaisamarmejia@gmail.com>
2022-06-10 17:49:28 -0700

    Update Youtube.jsx

--- src/components/youtube/Youtube.jsx
+++ src/components/youtube/Youtube.jsx
@@ -6,7 +6,7 @@ class Youtube extends Component {
     return (
       <div className={style.home} id="featured">
         <div className={style.videoContainer}>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/0UKiunYls-4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/nK_7FXdLZ_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       </div>
     );
