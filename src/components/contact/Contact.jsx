d1a4099f19e5450067c1b413c2e249474df23eb3
Linda Mejia <lindaisamarmejia@gmail.com>
2026-07-23 21:14:07 +0200

    Update contact information and layout in Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,38 +3,36 @@ import { Container, Row, Col } from "reactstrap";
 import style from "./Contact.module.css"
 
 class Contact extends Component {
-  render() {
-    return (
-      <div className={style.contact} id="contact">
-      <Container style={{ textAlign: "center" }} >
-        <Row>
-          <Col md={4}>
-            <a name="contact"></a>
-            <h3>MANAGEMENT</h3>
-            <a href="mailto:4B@ymugroup.com">4B@ymugroup.com</a>
-          </Col>
+render() {
+return (
+<div className={style.contact} id="contact">
+<Container style={{ textAlign: "center" }} >
+<Row>
+<Col md={6}>
+<a name="contact"></a>
+<h3>MANAGEMENT</h3>
+<a href="mailto:hrh@halembiz.com">hrh@halembiz.com</a>
+<br/>
+<a href="mailto:erin.lee@halembiz.com">erin.lee@halembiz.com</a>
+</Col>
 
-          <Col md={4}>
-            <h3>BOOKINGS</h3>
-            <h4>USA</h4>
-            <a class="two" href="mailto:agary@paradigmagency.com">
-              agary@paradigmagency.com{" "}
-            </a>
-          </Col>
-
-          <Col md={4}>
-            <h4>GLOBAL</h4>
-            <a class="three" href="mailto:paul@primarytalent.com">
-              paul@primarytalent.com{" "}
-            </a>
-          </Col>
-        </Row>
-      </Container>
-      <br/>
-     <p>Copyright © 2025 4B Touring LLC</p>
-      </div>
-    );
-  }
+<Col md={6}>
+<h3>BOOKINGS</h3>
+<a class="two" href="mailto:agary@the.team">
+agary@the.team{" "}
+</a>
+<br/>
+<a class="two" href="mailto:abby.ellis@the.team">
+abby.ellis@the.team{" "}
+</a>
+</Col>
+</Row>
+</Container>
+<br/>
+<p>Copyright © 2025 4B Touring LLC</p>
+</div>
+);
+}
 }
 
 export default Contact;
